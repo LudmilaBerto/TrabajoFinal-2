778194fc7c25ae32c11768eb85e4ed0637db8636
Ludmila Berto <ludmilaberto5@gmail.com>
2024-07-22 14:39:29 -0300

    Finalize la estructura jsx de la sectionDos y su estilizacion css con Chakra ui. Agregue una imagen a la carpeta img y la ixporte al componente SectionDos.jsx

--- trabajoFinalDos/src/Components/SectionDos/SectionDos.jsx
+++ trabajoFinalDos/src/Components/SectionDos/SectionDos.jsx
@@ -1,4 +1,5 @@
-import { Box, Divider, Flex, Text } from "@chakra-ui/react"
+import { Box, Divider, Flex, Image, Text } from "@chakra-ui/react"
+import CirculoContratame from '../../assets/img/Circulo Rosa Contrátame.svg'
 
 
 
@@ -56,20 +57,35 @@ const SectionDos=()=>{
             </Flex>
 
              {/* Separo con linea divisoria */}
-             <Divider orientation="horizontal" b='none' pt='1px' bg='#b9b7b7a2'/>
+            <Divider orientation="horizontal" b='none' pt='1px' bg='#b9b7b7a2'/>
              
             
             <Text className="ComillasGrandes" fontFamily='"Poppins", sans-serif' fontWeight='300' fontSize='100px' mt='60px'>“</Text>
 
             <Flex className="alineacion-h2-hr" align='center'>
                 
-                <Text color='#201f51' fontFamily='"Poppins", sans-serif' fontWeight='300' fontSize='28px' flexGrow='1'>Estoy estudiando una <Text as='span' fontWeight='500'>Diplomatura </Text>en <Text as='span' fontWeight='500'>Desarrollo Web Full Stack.</Text></Text><Divider orientation="horizontal" b='none' pt='1px' bg='#201f51'/>
+                <Text color='#201f51' fontFamily='"Poppins", sans-serif' fontWeight='300' fontSize='28px' flexBasis='80%'>Estoy estudiando una 
+                    <Text as='span' fontWeight='500'> Diplomatura </Text>
+                    en 
+                    <Text as='span' fontWeight='500'> Desarrollo Web Full Stack.</Text>
+                </Text>
+                <Divider orientation="horizontal" b='0px' pt='1px' bg='#201f51' flexBasis='20%'/>
+
+            </Flex>
+            
+            <Flex className="alineacion-h2-hr" align='center'>
+                
+                <Divider orientation="horizontal" b='none' pt='1px' bg='#201f51' flexBasis='20%'/><Text color='#201f51' fontFamily='"Poppins", sans-serif' fontWeight='300' fontSize='28px' flexBasis='80%' pl='15px'>Mi <Text as='span' fontWeight='500'>residencia</Text> actual está en la <Text as='span' fontWeight='500'>provincia de Buenos Aires, Argentina.</Text></Text>
 
             </Flex>
 
-            <Flex className="alineacion-h2-hr" align='center'>
+            <Flex className="contenedor-sticker-texto" align='center' py='30px'>
                 
-            <Divider orientation="horizontal" b='none' pt='1px' bg='#201f51' /><Text color='#201f51' fontFamily='"Poppins", sans-serif' fontWeight='300' fontSize='28px' flexGrow='1' pl='15px'>Mi <Text as='span' fontWeight='500'>residencia</Text> actual está en la <Text as='span' fontWeight='500'>provincia de Buenos Aires.</Text></Text>
+                <Image src={CirculoContratame} alt="imagen de circulo rosa con la palabra 'Contratame' " h='120px' px='80px' />
+
+                <Text fontFamily='"Barlow", sans-serif' fontWeight='400' lineHeight='14px' fontSize='13px' px='80px'>
+                     Al estudiar la Diplomatura en Desarrollo Web Full Stack, perfecciono mis habilidades técnicas y desarrollo un buen ojo para los detalles. Sin embargo, mi verdadera fortaleza radica en mis habilidades interpersonales, comunicación efectiva, adaptabilidad y mentalidad colaborativa. Prospero en entornos de equipo, busco constantemente oportunidades para aprender de mis compañeros y contribuir al éxito colectivo. Trabajar como autónomo me ha enseñado el valor de la gestión del tiempo, la autodisciplina y la capacidad de desempeñar múltiples funciones. Prontizar las tareas y cumplir los plazos con un compromiso inquebrantable.
+                </Text>
 
             </Flex>
 
